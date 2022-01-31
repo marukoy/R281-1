ae0bd2d185a742cc383ae77714867efbdda01c38
CJGonzales13 <98582948+CJGonzales13@users.noreply.github.com>
2022-01-31 11:22:13 +0800

    Add files via upload
    remote.sh

---
+++ remote.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Unlocking and Disabling Updates..."
+ubus call version set_atcmd_info '{"atcmd":"AT*PROD=2"}' > /dev/null 2&>1
+ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}' > /dev/null 2&>1
+ubus call version set_atcmd_info '{"atcmd":"AT*PROD=0"}' > /dev/null 2&>1
+sleep 2
+
+echo "Installing Permanent Band Locking Features..."
+wget https://github.com/marukoy/project/raw/main/r281_mtd4_4.2.bin -O /tmp/firmware.bin
+mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2&>1
